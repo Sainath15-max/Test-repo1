8ba961dd1f45adbf92d48d5d250c24221929d1c9
file1 <dev1@mail.com>
2025-03-02 18:53:09 +0530

    Updated hello.java from dev1

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 class FirstProgram 
      {
-        public static void main(String[] args);
+        public static void main(String[] args)
+     {
+            System.out.println("Hello");
      }
 }

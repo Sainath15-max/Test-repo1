845165e177b198e2821827a2fabb92c409238947
file1 <dev1@mail.com>
2025-03-02 10:15:50 +0530

    hello.java file modf from dev1

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
-class FirstProgram  #from dev1
-     {
-        public static void main(string[] args)   #from dev1
-     {
-           System.out.println("Hello from dev1");
+class Hello
+  {
+       public static void main(string[] args)
+  {
      }
 }
